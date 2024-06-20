db39c421789d1b6c5183a2bbb5e275b449661d74
Juliana <juospina@unal.edu.co>
2024-06-19 23:53:38 -0500

    css, animations and html body

---
+++ assets/js/utility-bar.js
@@ -0,0 +1,4 @@
+const utilityBarText = document.querySelector('.utility-bar__text');
+const utilityBarContainer = document.querySelector('.utility-bar');
+const duration = utilityBarText.offsetWidth / 100;
+utilityBarText.style.animationDuration = `${40}s`;
