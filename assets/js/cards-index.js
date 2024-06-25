29d9038f34e705e558ff33039511f0bc802bed05
Juliana <juospina@unal.edu.co>
2024-06-24 19:47:38 -0500

    js clothes responsive

--- assets/js/cards-index.js
+++ assets/js/cards-index.js
@@ -1,48 +1,53 @@
 const dataClothes = [
     {
+        id: 0,
         title: "Falda denim asimetric",
         price: "60.000",
-        image: "./assets/img/cards/chick-collection/asimetric-skirt/as-1.png",
+        image: "/img/cards/chick-collection/asimetric-skirt/as-1.png",
         alternative: "asimetric-skirt",
-        imageSecond: "./assets/img/cards/chick-collection/asimetric-skirt/as-2.png", 
-        imageThird: "./assets/img/cards/chick-collection/asimetric-skirt/as-3.png",
-        description: ""
+        imageSecond: "/img/cards/chick-collection/asimetric-skirt/as-2.png", 
+        imageThird: "/img/cards/chick-collection/asimetric-skirt/as-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Ceñido</li><li>Denim</li></ul>"
     },
     {
+        id: 1,
         title: "Hoodie pink star",
         price: "80.000",
-        image: "./assets/img/cards/pink-goth-collection/sweater-pink-star/ps-1.png",
+        image: "/img/cards/pink-goth-collection/sweater-pink-star/ps-1.png",
         alternative: "pink-star-hoodie",
-        imageSecond: "./assets/img/cards/pink-goth-collection/sweater-pink-star/ps-2.png", 
-        imageThird: "./assets/img/cards/pink-goth-collection/sweater-pink-star/ps-3.png",
-        description: ""
+        imageSecond: "/img/cards/pink-goth-collection/sweater-pink-star/ps-2.png", 
+        imageThird: "/img/cards/pink-goth-collection/sweater-pink-star/ps-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Oversized</li><li>Políester</li></ul>"
     },
     {
+        id: 2,
         title: "Sweater cozy scholar",
         price: "120.000",
-        image: "./assets/img/cards/cozy-collection/sweater-schoolar/gs-1.png",
+        image: "/img/cards/cozy-collection/sweater-schoolar/gs-1.png",
         alternative: "sweater-gray",
-        imageSecond: "./assets/img/cards/cozy-collection/sweater-schoolar/gs-2.png", 
-        imageThird: "./assets/img/cards/cozy-collection/sweater-schoolar/gs-3.png",
-        description: ""
+        imageSecond: "/img/cards/cozy-collection/sweater-schoolar/gs-2.png", 
+        imageThird: "/img/cards/cozy-collection/sweater-schoolar/gs-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Oversized</li><li>Algodón</li></ul>"
     },
     {
+        id: 3,
         title: "Vestido pink goth",
         price: "115.000",
-        image: "./assets/img/cards/pink-goth-collection/dress-squares-pink/sdp-1.png",
+        image: "/img/cards/pink-goth-collection/dress-squares-pink/sdp-1.png",
         alternative: "vestido-pink-goth",
-        imageSecond: "./assets/img/cards/pink-goth-collection/dress-squares-pink/sdp-2.png", 
-        imageThird: "./assets/img/cards/pink-goth-collection/dress-squares-pink/sdp-3.png",
-        description: ""
+        imageSecond: "/img/cards/pink-goth-collection/dress-squares-pink/sdp-2.png", 
+        imageThird: "/img/cards/pink-goth-collection/dress-squares-pink/sdp-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Ceñido</li><li>Políester</li></ul>"
     },
     {
+        id: 4,
         title: "Sweater pink goth",
         price: "80.000",
-        image: "./assets/img/cards/pink-goth-collection/sweater-first-love/flp-1.png",
+        image: "/img/cards/pink-goth-collection/sweater-first-love/flp-1.png",
         alternative: "sweater-pink-goth",
-        imageSecond: "./assets/img/cards/pink-goth-collection/sweater-first-love/flp-2.png", 
-        imageThird: "./assets/img/cards/pink-goth-collection/sweater-first-love/flp-3.png",
-        description: ""
+        imageSecond: "/img/cards/pink-goth-collection/sweater-first-love/flp-2.png", 
+        imageThird: "/img/cards/pink-goth-collection/sweater-first-love/flp-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Oversized</li><li>Políester</li></ul>"
     }
 ];
 
@@ -53,24 +58,38 @@ function cardsGenerate() {
         card.className = 'card';
         card.innerHTML = `
         <div>
-            <img src="${datos.image}" class="card-img-top first-image-1" alt="${datos.alternative}">
+            <img src="./assets${datos.image}" class="card-img-top first-image-1" alt="${datos.alternative}">
             <div class="card-body">
                 <div class="card-body__info">
                     <a href="./assets/pages/clothes.html" target="_blank">${datos.title}</a>
                     <p>$${datos.price}</p>
                 </div>
                 <div class="card-body__tag">
-                    <button>
+                    <button class="btn-view" data-id=${datos.id}>
                         <i class="fa-solid fa-tag fa-rotate-90"></i>
                     </button>
                 </div>
             </div>
         </div>
-    `;
-    container.appendChild(card);
+        `;
+        container.appendChild(card);
     
     });
+    btnView = document.querySelectorAll('.btn-view')
+    
+    btnView.forEach(btn => {
+        btn.addEventListener('click', (evento) => {
+            console.log(evento.target)
+            const index = evento.target.getAttribute('data-id')
+            const itemSelected = dataClothes.find(item => item.id == index);            
+            const strItemSelected = JSON.stringify(itemSelected)
+            localStorage.setItem('selected', strItemSelected)   
+        })
+    })
 }
+
+
+
 cardsGenerate();
 
 
--- assets/js/pages/cards.js
+++ assets/js/pages/cards.js
@@ -1,49 +1,54 @@
 const dataClothes = [
     {
+        id: 0,
         title: "Falda denim asimetric",
         price: "60.000",
-        image: "../img/cards/chick-collection/asimetric-skirt/as-1.png",
+        image: "/img/cards/chick-collection/asimetric-skirt/as-1.png",
         alternative: "asimetric-skirt",
-        imageSecond: "../img/cards/chick-collection/asimetric-skirt/as-2.png", 
-        imageThird: "../img/cards/chick-collection/asimetric-skirt/as-3.png",
-        description: ""
+        imageSecond: "/img/cards/chick-collection/asimetric-skirt/as-2.png", 
+        imageThird: "/img/cards/chick-collection/asimetric-skirt/as-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Ceñido</li><li>Denim</li></ul>"
     },
     {
+        id: 1,
         title: "Hoodie pink star",
         price: "80.000",
-        image: "../img/cards/pink-goth-collection/sweater-pink-star/ps-1.png",
+        image: "/img/cards/pink-goth-collection/sweater-pink-star/ps-1.png",
         alternative: "pink-star-hoodie",
-        imageSecond: "../img/cards/pink-goth-collection/sweater-pink-star/ps-2.png", 
-        imageThird: "../img/cards/pink-goth-collection/sweater-pink-star/ps-3.png",
-        description: ""
+        imageSecond: "/img/cards/pink-goth-collection/sweater-pink-star/ps-2.png", 
+        imageThird: "/img/cards/pink-goth-collection/sweater-pink-star/ps-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Oversized</li><li>Políester</li></ul>"
     },
     {
+        id: 2,
         title: "Sweater cozy scholar",
         price: "120.000",
-        image: "../img/cards/cozy-collection/sweater-schoolar/gs-1.png",
+        image: "/img/cards/cozy-collection/sweater-schoolar/gs-1.png",
         alternative: "sweater-gray",
-        imageSecond: "../img/cards/cozy-collection/sweater-schoolar/gs-2.png", 
-        imageThird: "../img/cards/cozy-collection/sweater-schoolar/gs-3.png",
-        description: ""
+        imageSecond: "/img/cards/cozy-collection/sweater-schoolar/gs-2.png", 
+        imageThird: "/img/cards/cozy-collection/sweater-schoolar/gs-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Oversized</li><li>Algodón</li></ul>"
     },
     {
+        id: 3,
         title: "Vestido pink goth",
         price: "115.000",
-        image: "../img/cards/pink-goth-collection/dress-squares-pink/sdp-1.png",
+        image: "/img/cards/pink-goth-collection/dress-squares-pink/sdp-1.png",
         alternative: "vestido-pink-goth",
-        imageSecond: "../img/cards/pink-goth-collection/dress-squares-pink/sdp-2.png", 
-        imageThird: "../img/cards/pink-goth-collection/dress-squares-pink/sdp-3.png",
-        description: ""
+        imageSecond: "/img/cards/pink-goth-collection/dress-squares-pink/sdp-2.png", 
+        imageThird: "/img/cards/pink-goth-collection/dress-squares-pink/sdp-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Ceñido</li><li>Políester</li></ul>"
     },
     {
+        id: 4,
         title: "Sweater pink goth",
         price: "80.000",
-        image: "../img/cards/pink-goth-collection/sweater-first-love/flp-1.png",
+        image: "/img/cards/pink-goth-collection/sweater-first-love/flp-1.png",
         alternative: "sweater-pink-goth",
-        imageSecond: "../img/cards/pink-goth-collection/sweater-first-love/flp-2.png", 
-        imageThird: "../img/cards/pink-goth-collection/sweater-first-love/flp-3.png",
-        description: ""
-    },
+        imageSecond: "/img/cards/pink-goth-collection/sweater-first-love/flp-2.png", 
+        imageThird: "/img/cards/pink-goth-collection/sweater-first-love/flp-3.png",
+        description: "<ul><li>Estilo Y2k</li><li>Oversized</li><li>Políester</li></ul>"
+    }
 ];
 
 function cardsGenerate() {
@@ -53,25 +58,101 @@ function cardsGenerate() {
         card.className = 'card';
         card.innerHTML = `
         <div>
-            <img src="${datos.image}" class="card-img-top first-image-1" alt="${datos.alternative}">
+            <img src="..${datos.image}" class="card-img-top first-image-1" alt="${datos.alternative}">
             <div class="card-body">
                 <div class="card-body__info">
                     <a href="../pages/clothes.html" target="_blank">${datos.title}</a>
                     <p>$${datos.price}</p>
                 </div>
                 <div class="card-body__tag">
-                    <button>
+                    <button class="btn-view" data-id=${datos.id}>
                         <i class="fa-solid fa-tag fa-rotate-90"></i>
                     </button>
                 </div>
             </div>
         </div>
-    `;
-    container.appendChild(card);
+        `;
+        container.appendChild(card);
     
     });
 }
+
 cardsGenerate();
+
+
+
+const selectedItem = localStorage.getItem('selected')
+const parsedItem = JSON.parse(selectedItem)
+const arrayItem = []
+arrayItem.push(parsedItem)
+console.log(parsedItem)
+console.log(parsedItem.title)
+
+
+function addHTMLClothes() {
+    const element = document.getElementById('clothe__images');
+    if (element) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="carousel-item active">
+                <img src="..${parsedItem.image}" class="d-block w-100" alt="${parsedItem.alternative}">
+            </div>
+            <div class="carousel-item">
+                <img src="..${parsedItem.imageSecond}" class="d-block w-100" alt="${parsedItem.alternative}">
+            </div>
+            <div class="carousel-item">
+                <img src="..${parsedItem.imageThird}" class="d-block w-100" alt="${parsedItem.alternative}">
+            </div>
+        `;
+        while (div.firstChild) {
+            element.appendChild(div.firstChild);
+        }
+    } else {
+        console.error(`Elemento con ID "${elementoId}" no encontrado.`);
+    }
+}
+addHTMLClothes();
+
+function addHTMLClothesTitle() {
+    const element = document.getElementById('clothe__title');
+    if (element) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <h2>
+                ${parsedItem.title}
+            </h2>
+            <p>
+                ${parsedItem.price}
+            </p>
+        `;
+        while (div.firstChild) {
+            element.appendChild(div.firstChild);
+        }
+    } else {
+        console.error(`Elemento con ID "${elementoId}" no encontrado.`);
+    }
+}
+addHTMLClothesTitle();
+
+function addHTMLClothesDescription() {
+    const element = document.getElementById('clothe__description');
+    if (element) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <p>
+                ${parsedItem.description}
+            </p>
+        `;
+        while (div.firstChild) {
+            element.appendChild(div.firstChild);
+        }
+    } else {
+        console.error(`Elemento con ID "${elementoId}" no encontrado.`);
+    }
+}
+addHTMLClothesDescription();
+
+
 
 
 
